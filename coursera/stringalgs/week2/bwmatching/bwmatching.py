6287cc9a4e308bbf9f464587fc8d04bc524d6b72
Vitalie Cracan <vitalie.cracan@gmail.com>
2019-05-31 16:36:28 +0300

    completed week 2

--- coursera/stringalgs/week2/bwmatching/bwmatching.py
+++ coursera/stringalgs/week2/bwmatching/bwmatching.py
@@ -30,45 +30,41 @@ def build_count_occurence_dict(lastCol, firstOccurenceDict):
   return count_occurence
 
 
-def PreprocessBWT(bwt):
-  """
-  Preprocess the Burrows-Wheeler Transform bwt of some text
-  and compute as a result:
-    * starts - for each character C in bwt, starts[C] is the first position 
-        of this character in the sorted array of 
-        all characters of the text.
-    * occ_count_before - for each character C in bwt and each position P in bwt,
-        occ_count_before[C][P] is the number of occurrences of character C in bwt
-        from position 0 to position P inclusive.
-  """
-  # Implement this function yourself
-  pass
+def count_pattern_occurence(pattern, bwt, firstOccurence, countOccurence):
+  top = 0
+  bottom = len(bwt) - 1
+  
+  while top <= bottom:
+      if pattern == "":
+          return bottom - top + 1
+      else:
+          char = pattern[-1]
+          if char not in firstOccurence:
+              return 0
 
-
-def CountOccurrences(pattern, bwt, starts, occ_counts_before):
-  """
-  Compute the number of occurrences of string pattern in the text
-  given only Burrows-Wheeler Transform bwt of the text and additional
-  information we get from the preprocessing stage - starts and occ_counts_before.
-  """
-  # Implement this function yourself
-  return 0
+          pattern = pattern[:-1]
+          count_top = countOccurence[char][top]
+          count_bottom = countOccurence[char][bottom+1]
+          if count_bottom > count_top:
+              top = firstOccurence[char] + count_top
+              bottom = firstOccurence[char] + count_bottom - 1
+          else:
+              return 0
      
 
 
 if __name__ == '__main__':
   bwt = sys.stdin.readline().strip()
-  # pattern_count = int(sys.stdin.readline().strip())
-  # patterns = sys.stdin.readline().strip().split()
-
-  # starts, occ_counts_before = PreprocessBWT(bwt)
-  # occurrence_counts = []
-  # for pattern in patterns:
-  #   occurrence_counts.append(CountOccurrences(pattern, bwt, starts, occ_counts_before))
-  # print(' '.join(map(str, occurrence_counts)))
-
+  pattern_count = int(sys.stdin.readline().strip())
+  patterns = sys.stdin.readline().strip().split()
+  
   first_col = sorted(bwt)
   first_occurence = build_first_occurence_dict(first_col)
   count_occurence = build_count_occurence_dict(bwt, first_occurence)
-  print(first_occurence)
-  print(count_occurence)
+  # print(first_occurence)
+#   print(count_occurence)
+
+  occurrence_counts = []
+  for pattern in patterns:
+    occurrence_counts.append(count_pattern_occurence(pattern, bwt, first_occurence, count_occurence))
+  print(' '.join(map(str, occurrence_counts)))
