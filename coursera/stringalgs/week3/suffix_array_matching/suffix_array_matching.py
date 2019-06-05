194e9ad9086a7784e6b4a25b027d17d4daab2417
Vitalie Cracan <vitalie.cracan@endava.com>
2019-06-05 13:08:53 +0300

    suffix array matching

--- coursera/stringalgs/week3/suffix_array_matching/suffix_array_matching.py
+++ coursera/stringalgs/week3/suffix_array_matching/suffix_array_matching.py
@@ -1,15 +1,130 @@
 # python3
 import sys
 
+def build_char_order(text):
+    L = len(text)
+    alphabet = {'$': 0, 'A': 1, 'C': 2, 'G': 3, 'T': 4}
+    A = len(alphabet)
+    count = [0] * A
+    order = [0] * L
+    
+    for i in range(L):
+        char = text[i]
+        count[alphabet[char]] += 1
+    
+    # convert counts to tails
+    for i in range(1, A):
+        count[i] += count[i-1]
+    
+    for i in reversed(range(0, L)):
+        char = text[i]
+        j = alphabet[char]
+        count[j] -= 1
+        order[count[j]] = i
+        
+    
+    return order
+        
+def build_char_classes(text, order):
+    L = len(text)
+    classes = [0] * L
+    A = 0
+    
+    for i in range(1, L):
+        if text[order[i]] != text[order[i-1]]:
+            A += 1
+        
+        classes[order[i]] = A
+    
+    return classes, A + 1
+        
+
+def build_doubled_order(text, N, order, classes, A):
+    L = len(text)
+    new_order = [0] * L
+    count = [0] * A
+    
+    for i in range(L):
+        count[classes[i]] += 1
+    
+    for i in range(1, A):
+        count[i] += count[i-1]
+    
+    for i in reversed(range(L)):
+        j = (order[i] - N + L) % L
+        cl = classes[j]
+        count[cl] -= 1
+        new_order[count[cl]] = j
+    
+    return new_order
+
+def build_doubled_classes(doubledOrder, N, classes):
+    A = 0
+    L = len(doubledOrder)
+    new_classes = [0] * L
+    
+    for i in range(1, L):
+        cur = doubledOrder[i]
+        prev = doubledOrder[i-1]
+        mid = (cur + N) % L
+        mid_prev = (prev + N) % L
+        if classes[cur] != classes[prev] or classes[mid] != classes[mid_prev]:
+            A += 1
+        new_classes[cur] = A
+    
+    return new_classes, A+1  
+   
+def build_suffix_array(text):
+    L = len(text)
+    order = build_char_order(text)
+    classes, A = build_char_classes(text, order)
+    N = 1
+    while N < L: 
+        order = build_doubled_order(text, N, order, classes, A)
+        classes, A = build_doubled_classes(order, N, classes)
+        N *= 2
+    
+    return order
+
 def find_occurrences(text, patterns):
+    L = len(text)
+    sa = build_suffix_array(text)
+    #print(sa)
     occs = set()
 
-    // write your code here
+    for p in patterns:
+        left = 0
+        right = L
+        M = len(p)
+        while left < right:
+            mid = (left + right) // 2
+            suffix = sa[mid]
+            if p > text[suffix:M+suffix]:
+                left = mid + 1
+            else:
+                right = mid
+        
+        start = left
+        right = L
+
+        while left < right:
+            mid = (left + right) // 2
+            suffix = sa[mid]
+            if p < text[suffix:M+suffix]:
+                right = mid
+            else:
+                left = mid + 1
+
+        end = right
+
+        for i in range(start, end):
+            occs.add(sa[i])
 
     return occs
 
 if __name__ == '__main__':
     text = sys.stdin.readline().strip()
+    text += '$'
     pattern_count = int(sys.stdin.readline().strip())
     patterns = sys.stdin.readline().strip().split()
     occs = find_occurrences(text, patterns)
